0df32a10094a5390eb39431c59dce5e7911e7413
Everton Borges <evertonsoaresborges@gmail.com>
2021-01-30 23:01:36 -0400

    feat: add scenario, and interaction with key

---
+++ Assets/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+
+    [Range(0.01f, 0.99f)]
+    [SerializeField] private float crounchSpeedFactor = 0.5f;
+    public float speed = 5;
+    Vector2 velocity;
+
+    private Crouch _crouch = null;
+
+    private Key _keyReference = null;
+
+    void Awake() 
+    {
+        _crouch = GetComponentInChildren<Crouch>();
+    }
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0f)));
+        if (Physics.Raycast(ray, out RaycastHit info, 0.75f, 1 << LayerMask.NameToLayer("Key")))
+        {
+            var key = info.transform.GetComponent<Key>();
+            if (key)
+            {
+                _keyReference = key;
+                _keyReference.Show();
+            }
+        }
+        else
+        {
+            if (_keyReference)
+            {
+                _keyReference.Hide();
+                _keyReference = null;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float finalSpeed = speed * (_crouch.IsCrouched ? crounchSpeedFactor : 1f);
+
+        velocity.y = Input.GetAxis("Vertical") * finalSpeed * Time.deltaTime;
+        velocity.x = Input.GetAxis("Horizontal") * finalSpeed * Time.deltaTime;
+        transform.Translate(velocity.x, 0, velocity.y);
+    }
+}
---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Key : MonoBehaviour
+{
+    
+    [SerializeField] private Image _interactImage = null;
+
+    private bool _isShowing = false;
+
+    void Awake() 
+    {
+        _interactImage.gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        if (!_isShowing)
+        {
+            _isShowing = true;
+            _interactImage.transform.position = transform.position + Vector3.up * 0.175f + Vector3.left * 0.05f;
+            _interactImage.gameObject.SetActive(_isShowing);
+        }
+    }
+
+    public void Hide()
+    {
+        if (_isShowing)
+        {
+            _isShowing = false;
+            _interactImage.gameObject.SetActive(_isShowing);
+        }
+    }
+
+}
